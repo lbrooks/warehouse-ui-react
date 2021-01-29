85e87beaffd8cb645c41059cf397cb5efb717786
Lawrence Brooks <84.lbrooks@gmail.com>
2021-01-28 21:43:37 -0500

    add to tracing

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ function initTracer(serviceName) {
   var config = {
     serviceName: serviceName,
     sampler: {
-      type: "const",
-      param: 1,
+        type: "const",
+        param: 1,
     },
     reporter: {
-        collectorEndpoint: process.env.JAEGER_URL //'http://localhost:14268/api/traces',
+        collectorEndpoint: process.env.JAEGER_URL || 'http://localhost:14268/api/traces'
     },
   };
   var options = {
@@ -34,7 +34,22 @@ function initTracer(serviceName) {
 
 const tracer = initTracer('warehouse-react');
 
-app.use('/api', createProxyMiddleware({ target: process.env.WAREHOUSE_URL, changeOrigin: true })); //'http://localhost:3001'
+app.use('/api/*', (req, res, next) => {
+    const span = tracer.startSpan(req.baseUrl);
+
+    for(let i = 0; i < req.rawHeaders.length; i += 2){
+        span.setTag(`request.header.${req.rawHeaders[i]}`, req.rawHeaders[i+1]);
+    }
+    span.setTag(`request.query`, req.query);
+
+    res.on('finish', () => {
+        span.setTag(`http.response_code`, res.statusCode);
+        span.finish();
+    });
+
+    next();
+});
+app.use('/api', createProxyMiddleware({ target: process.env.WAREHOUSE_URL || 'http://localhost:3001', changeOrigin: true }));
 app.use(express.static('build'))
 
 app.listen(port, () => {
